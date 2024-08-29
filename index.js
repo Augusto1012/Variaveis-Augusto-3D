44c89454a07767a6ac2272e9ebb54dee5152fcb8
Augusto <127844983+Augusto1012@users.noreply.github.com>
2024-08-29 01:30:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoAtual = 2024;
+const nomeCompleto = "Augusto Fortunato de Lima Inácio";
+let idade = 16;
+const cursoFaculdade = "Programação";
+let cidadeAtual = "SJP";
+
+console.log(`ola eu sou o ${nomeCompleto} eu tenho ${idade} anos, moro em ${cidadeAtual} quero ano que vem estar cursando ${cursoFaculdade}.`);
+
+anoAtual = 2025;
+idade = 17;
+
+console.log(`No ano que vem meu nome ainda será ${nomeCompleto},terei completado meus ${idade} anos, pretendo estar morando ainda em ${cidadeAtual} e espero estar me dando bem na faculdade de ${cursoFaculdade}.`);
+
